3762ba3b01839f629b861764aeaa2efd453720a7
Aristide Fumagalli <aristide1997@gmail.com>
2024-03-14 17:21:45 +0000

    dev

--- kubernetes_control_plane_fix_scenario/step2/background.sh
+++ kubernetes_control_plane_fix_scenario/step2/background.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Introduce a misconfiguration in the etcd manifest
-sed -i 's/--listen-client-urls=https:\/\/127.0.0.1:2379/--listen-client-urls=https:\/\/127.0.0.1:12379/g' /etc/kubernetes/manifests/etcd.yaml
+sed -i 's+/etc/kubernetes/pki/etcd/peer.crt+/etc/kubernetes/file/pki/etcd/peer.crt+' /etc/kubernetes/manifests/etcd.yaml
 systemctl restart kubelet
